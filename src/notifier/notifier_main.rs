2c556242cd9cffb24fcfa3506588574e6e800ff2
Kastriot Kastrati <kastriotrkastrati@gmail.com>
2023-10-29 21:38:11 +0100

    todo: mark notification as todo

--- src/notifier/notifier_main.rs
+++ src/notifier/notifier_main.rs
@@ -1,9 +1,3 @@
-use notify_rust;
-
 pub fn notify() {
-    let _ = notify_rust::Notification::new()
-        .summary("Firefox News")
-        .body("This will almost look like a real firefox notification.")
-        .icon("firefox")
-        .show();
+    todo!()
 }
